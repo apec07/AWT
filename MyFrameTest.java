59c34d796507f538a55f9641a6f86460823002d1
apec07 <apec07@gmail.com>
2020-06-30 22:56:06 +0800

    modified with textfield

--- MyFrameTest.java
+++ MyFrameTest.java
@@ -1,26 +1,52 @@
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.JOptionPane;
 
 public class MyFrameTest extends Frame implements ActionListener {
 	
 	private String name;
+	private Button saveBtn,exitBtn;
+	private TextField inputField;
 	
 	//Contructor
 	public MyFrameTest(String name){
 		this.name = name;
 		setLocation(10,10);
 		setSize(200,300);
-		Panel pan = new Panel();
-			Button clearBtn = new Button ("Exit");	
-			clearBtn.addActionListener (this);	
-			pan.add (clearBtn);					
-			add (pan, BorderLayout.NORTH);
+		setLayout (new GridLayout(3,1));	
+		//Panel pan = new Panel();
+		  inputField = new TextField();
+		  inputField.setText("Please type in here");
+		  inputField.addActionListener (this);	
+			exitBtn = new Button ("Exit");	
+			exitBtn.addActionListener (this);	
+			saveBtn = new Button ("Save");	
+			saveBtn.addActionListener (this);	
+			add(inputField);
+			add(saveBtn);
+			add(exitBtn);
+	
 	}
 	//overwrited ActionListener method
 	public void actionPerformed (ActionEvent e) {
-		setVisible (false);
-	  dispose();
-	  //System.exit (0);
+			if (e.getSource() == exitBtn){
+				//JOptionPane.showMessageDialog(null, "Exit");
+				int option =JOptionPane.showConfirmDialog(null, "Really Exit?", "Exit", JOptionPane.YES_NO_OPTION);
+				System.out.println(option);
+				if(option==1){
+					//Do NOT EXIT!
+					return;
+				}
+				setVisible (false);
+	  		dispose();
+	  		//System.exit (0);
+			} else if (e.getSource() == saveBtn) {
+				JOptionPane.showMessageDialog(null, "Saved...");  
+				//saveText();
+			} else{
+				System.err.println("Not defiend");
+			}
+		
 	}
 	
 	//Start here
